b12eb6dff67431259ad7f7de7926fa30e51a2e4b
CSMathematics <spyrosfronimos@gmail.com>
2024-09-27 10:19:03 +0300

    Βάσεις δεδομένων DataTex

---
+++ Βάσεις Δεδομένων DataTeX/Παλιά ΒΔ - Β Λυκείου/b.sql
@@ -0,0 +1,37 @@
+%# Database File : DTX-Alg-Syst-GrSys-MethAnt-SectEx1
+%@ Database source: B_Lykeiou
+Να λυθεί το παρακάτω σύστημα με τη μέθοδο της αντικατάσταση
+\[ \ccases{2x+y=5\\3x-2y=4} \]
+Νέο κείμενο
+%# End of file DTX-Alg-Syst-GrSys-MethAnt-SectEx1
+%# Database File : DTX-AnGeom-Dian-SyntD-SyntDAkr-SectEx1
+%@ Database source: B_Lykeiou
+Δίνονται τα σημεία $ A(-2,3),B(1,4) $ και $ \varGamma(5,3) $.
+\begin{alist}
+\item Να βρεθούν οι συντεταγμένες των διανυσμάτων $ \overarrow{AB},\overarrow{A\varGamma} $.
+\item Να βρεθούν οι συντεταγμένες του διανύσματος $ \overarrow{BM} $ όπου $ M $ είναι το μέσο του ευθ'υγραμμου τμήματος $ A\varGamma $.
+\end{alist}
+%# End of file DTX-AnGeom-Dian-SyntD-SyntDAkr-SectEx1
+%# Database File : DTX-AnGeom-Conic-Kykl-ExKyklou-SectEx1
+%@ Database source: B_Lykeiou
+Σε καθένα από τα παρακάτω ερωτήματα να βρείτε την εξίσωση του κύκλου:
+\begin{alist}
+\item με κέντρο $ K(-3,2) $ και ακτίνα $ \rho=4 $.
+\item με διάμετρο $ AB $ όπου $ A(1,0) $ και $ B(3,4) $.
+\item με κέντρο $ Κ(2,4) $ και διέρχεται από το σημείο $ A(5,0) $.
+\item με κέντρο $ K(-1,2) $ και εφαπτομένη την ευθεία $ \varepsilon: x+2y-2=0 $.
+\end{alist}
+%# End of file DTX-AnGeom-Conic-Kykl-ExKyklou-SectEx1
+%# Database File : DTX-Alg-Syst-GrSys-Param-SectEx1
+%@ Database source: B_Lykeiou
+Να βρεθούν οι τιμές της παραμέτρου $ \lambda\in\mathbb{R} $ ώστε η ευθεία $ (\lambda^2-1)x+(1-\lambda)y=2 $ να διέρχεται από το σημείο $ A(1,3) $.
+%# End of file DTX-Alg-Syst-GrSys-Param-SectEx1
+%# Database File : DTX-Alg-Syst-GrSys-Param-SectEx2
+%@ Database source: B_Lykeiou
+Να βρεθούν οι τιμές της παραμέτρου $ \lambda\in\mathbb{R} $ ώστε η ευθεία $ \lambda x+(\lambda-1)y=4 $ να διέρχεται από το σημείο $ A(-2,3) $.
+%# End of file DTX-Alg-Syst-GrSys-Param-SectEx2
+%# Database File : DTX-Alg-Syst-GrSys-Param-SectEx3
+%@ Database source: B_Lykeiou
+Αν γνωρίζουμε ότι το σημείο $ A(3\lambda-1,4-\lambda) $ ανήκει στην ευθεία $ 2x+3y=1 $ τότε να βρεθούν οι τιμές της παραμέτρου $ \lambda\in\mathbb{R} $.
+%# End of file DTX-Alg-Syst-GrSys-Param-SectEx3
+
