52dd9639cb217d620a34f294fe696d5859fdfef8
CSMathematics <spyrosfronimos@gmail.com>
2025-11-17 22:03:43 +0200

    Αλλαγή δομής φακέλων

--- LuaLaTeX/Δοκιμές Lualatex/test.lua
+++ LuaLaTeX/Δοκιμές Lualatex/test.lua
@@ -1,7 +1,7 @@
 function trigtable ()
     for t=0, 45, 1 do
         x=math.rad(t)
-        tex.print(string.format([[%2d$^{\circ}$ & %1.2f & %1.2f & %1.2f & %1.2f \\]],
-                                t, x, math.sin(x), math.cos(x), math.tan(x)))
+        tex.print(string.format([[%2d$^{\circ}$ & %1.2f & %1.4f & %1.4f & %1.4f & %1.4f \\]],
+                                t, x, math.sin(x), math.cos(x), math.tan(x), 1/math.tan(x)))
     end
 end
--- Πακέτα LaTeX - Εντολές/Autocomplete/user/aniswseis.cwl
+++
@@ -1,5 +0,0 @@
-\Xapeiro{arg1}{arg2}{arg3}{arg4}{arg5}
-\apeiroX{arg1}{arg2}{arg3}{arg4}{arg5}
-\axonas{arg1}{arg2}{arg3}
-\diasthma{arg1}{arg2}{arg3}{arg4}{arg5}{arg6}{arg7}
-\oria{katw}{anw1}{anw2}
